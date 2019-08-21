662b78f0e6a1c953f791936fa202e23792a93012
Santiago Lorenzo <santylore4@gmail.com>
2019-08-20 21:30:41 -0300

    Improvement in 1# problem

--- Coding Problems/JavaScript/Arrays/1#.js
+++ Coding Problems/JavaScript/Arrays/1#.js
@@ -4,32 +4,8 @@
 // For example, given [10, 15, 3, 7] and k of 17,
 //return true since 10 + 7 is 17.
 
-//======================================>
-
 let arr = [1, 2, 3, 4, 5];
 let k = 0;
-
-// solution with 2 for
-
-function sum(arr, k) {
-  let res = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (k > arr[i]) {
-      res = k - arr[i];
-      for (let j = 0; j < arr.length; j++) {
-        if (arr[j] === res) {
-          return true;
-        }
-      }
-    }
-  }
-  return false;
-}
-console.log(sum(arr, k));
-
-//======================================>
-
-// solution with 1 for
 
 const firulais = (arr, k) => {
   // creo un nuevo set
@@ -48,3 +24,5 @@ const firulais = (arr, k) => {
 };
 
 console.log(firulais(arr, k));
+
+// This problem is from https://www.dailycodingproblem.com/
